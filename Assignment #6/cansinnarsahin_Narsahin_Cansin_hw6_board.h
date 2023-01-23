1522c67008830dbcc2b7dcc35a1132a724df8670
Cansın Narşahin <79102795+cansnnnn@users.noreply.github.com>
2023-01-23 14:14:03 +0300

    Add files via upload

---
+++ Assignment #6/cansinnarsahin_Narsahin_Cansin_hw6_board.h
@@ -0,0 +1,43 @@
+//Cansin Narsahin
+//29126
+
+#include <iostream>
+#include <string>
+
+#ifndef cansinnarsahin_Narsahin_Cansin_hw6_board_H
+#define cansinnarsahin_Narsahin_Cansin_hw6_board_H
+
+template <class Type> struct cell
+{
+	Type value;
+	int id;
+};
+
+
+template <class Type> 
+class Board
+{
+	private:
+        int row;
+		int col;
+        cell<Type> **myBoard; //matrix for board
+
+	public:
+		Board(int, int );			//construct the board with no owners of the cells 
+		Board(const Board<Type> & );				// Deep Copy Constructor
+		~Board();									// Destructor 
+
+		void displayBoard() const;					//Displays the board
+		void updateBoardCells(int, Type);	//populate all the cells owned by that player with that value
+		int getPlayerCellsCount(int) const;		//returns the number of cells that player owns
+		int knowTheOwner(int, int);				//returns the owner of that cell
+
+		bool update(int, int, Type, int);	//modfy the value of cell
+		bool unclaim(int, int,int);				//set id 0
+		int get_row();
+		int get_col();
+};
+
+#include "cansinnarsahin_Narsahin_Cansin_hw6_board.cpp"
+
+#endif
